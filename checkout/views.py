05d5231175757477602f92c4e9c679ae879c9e65
mayasaffron <mayasaffronhan@hotmail.com>
2021-01-31 18:30:18 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -10,12 +10,14 @@ def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "the bag is empty, keep shopping!")
-        return redirect (reverse('products'))
+        return redirect(reverse('products'))
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IFjjIIPA0UV9eVp4bqR4o9gSIiRfGDzBpzMvO3JC0oKyyKa3gfDF34xcuIQCtRGwDIh5AAg2ms6iM0Ny1O4cxxg00iwEX8n1h',
+        'client_secret': 'test client secret',
     }
 
-    return render (request, template, context)
+    return render(request, template, context)
