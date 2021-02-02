a0b32351fa7c762fdccfa72170df8762fb6c29a1
mayasaffron <mayasaffronhan@hotmail.com>
2021-02-02 16:40:59 +0000

    added web hook listener

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -8,7 +8,21 @@ class StripeWH_Handler:
     def handle_event(self, event):
         ''' handle generic/unexpected webhook event'''
         return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        ''' handle payment intent.succeeded web hook from
+        stripe
+        '''
+        return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
 
-
+    def handle_payment_intent_failed(self, event):
+        ''' handle payment intent.succeeded web hook from
+        stripe
+        '''
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
