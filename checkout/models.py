96dbe9b896c3842f9774cf86046fef0ac827f2ec
mayasaffron <mayasaffronhan@hotmail.com>
2021-02-02 12:54:48 +0000

    added checkout success logic

--- checkout/models.py
+++ checkout/models.py
@@ -28,8 +28,11 @@ class Order(models.Model):
         return uuid.uuid4().hex.upper()
 
     def update_total(self):
-        ''' Update grand total each time a line item is added, accounting for delivery costs. '''
-        self.order_total = self.lineitems.aggregate(sum('lineitem_total'))['lineitem_total__sum']
+        ''' Update grand total each time a line item is added,
+            accounting for delivery costs. 
+        '''
+        self.order_total = self.lineitems.aggregate(
+                                                    Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE/100
         else:
@@ -38,9 +41,12 @@ class Order(models.Model):
         self.save()
 
     def save(self, *args, **kwargs):
-        ''' Override the default save method to set the order number, if it hasnt been set already '''
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
         if not self.order_number:
-            self.order_number = self._generate_order_number
+            self.order_number = self._generate_order_number()
         super().save(*args, **kwargs)
 
     def __str__(self):
@@ -50,7 +56,7 @@ class Order(models.Model):
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
+    product_size = models.CharField(max_length=2, null=True, blank=True)  # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
