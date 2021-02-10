37d1630716ea43afaf0d14c8264f17d1ddf6be7c
mayasaffron <mayasaffronhan@hotmail.com>
2021-02-10 14:05:59 +0000

    added delete functionality for super user

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('add/', views.add_product, name='add_product'),
     path('update/<int:product_id>/', views.update_product,
          name='update_product'),
+    path('delete/<int:product_id>/', views.delete_product,
+         name='delete_product'),
 ]
