a2b390d59c1cf162e2057c9a64fe66b1ab7acb15
mayasaffron <mayasaffronhan@hotmail.com>
2021-02-11 12:26:18 +0000

    added the procfile and disabled heroku from collecting the static files on deployment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'xv9d6amr$nos(q$y!uxxc)2$g*o^#=4&m=xb%ba+0sic488^nd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['boutique-ado-maya.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -108,13 +108,20 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+
+else:
+    DATABASES = {
+   'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
