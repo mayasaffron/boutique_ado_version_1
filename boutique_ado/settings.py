9efac986c861b22a764241c7c039ff030a1be16c
mayasaffron <mayasaffronhan@hotmail.com>
2021-02-13 11:21:34 +0000

    changed email host password variable name

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -206,6 +206,6 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp@gmail.com'
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
